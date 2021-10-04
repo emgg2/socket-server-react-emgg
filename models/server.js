1c030f72d2c31681e85f0de1f6efa9c6fce5b16f
Eva Garcia <emgg2222@gmail.com>
2021-10-04 17:53:02 +0200

    add CORS

--- models/server.js
+++ models/server.js
@@ -4,6 +4,7 @@ const http = require('http');
 const socketio = require('socket.io');
 const path = require('path');
 const Sockets = require('./sockets');
+const cors = require('cors');
 
 
 
@@ -21,6 +22,10 @@ class Server {
     
     middlewares() {        
         this.app.use(express.static( path.resolve(__dirname, '../public') ));
+
+        //CORS
+
+        this.app.use(cors());
     }
 
     configurarSockets(){
